91f489b32ced564e1ba1baa40cd4d9a41f854d6a
dinht <dinht@DESKTOP-KEIC5V2>
2025-06-09 14:20:04 +0700

    Add project files.

---
+++ UploadExcel/Controllers/UploadExcelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UploadExcel.PublicClasses;
+
+namespace UploadExcel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadExcelController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public IActionResult UploadFile(IFormFile file)
+        {
+            return Ok(new UploadHandler().Upload(file));
+        }
+    }
+}
---
+++ UploadExcel/PublicClasses/UploadHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace UploadExcel.PublicClasses
+{
+    public class UploadHandler
+    {
+        public string Upload(IFormFile file)
+        {
+            //extension
+            List<string> allowedExtensions = new List<string> { ".xls", ".xlsx" };
+            string extension = Path.GetExtension(file.FileName);
+
+            if (allowedExtensions.Contains(extension))
+            {
+                return $"File extension is valid. ({string.Join(',', allowedExtensions)})";
+            }
+
+            //file size
+            long size = file.Length;
+            if (size > (50 * 1024 * 1024)) // 50 MB
+            {
+                return "File size exceeds the limit of 50 MB.";
+            }
+
+            //name changing
+            string newFileName = Guid.NewGuid().ToString() + extension;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+            using FileStream stream = new FileStream(Path.Combine(path, newFileName), FileMode.Create);
+            file.CopyTo(stream);
+
+            return newFileName;
+        }
+    }
+}
